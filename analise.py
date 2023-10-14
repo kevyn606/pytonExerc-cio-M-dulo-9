713b1d286bb329c1a646cb4179f514ad0080ad08
kevyn 60 <kevynsousa606@gmail.com>
2023-10-13 23:50:55 -0300

    first commit

---
+++ analise.py
@@ -0,0 +1,47 @@
+import os
+import time
+import json
+from random import random
+from datetime import datetime
+import requests
+import csv
+from sys import argv
+import pandas as pd
+import seaborn as sns
+
+URL = 'https://www2.cetip.com.br/ConsultarTaxaDi/ConsultarTaxaDICetip.aspx'
+
+for _ in range(0, 10):
+    data_e_hora = datetime.now()
+    data = datetime.strftime(data_e_hora, '%Y/%m/%d')
+    hora = datetime.strftime(data_e_hora, '%H:%M:%S')
+
+    try:
+        response = requests.get(URL)
+        response.raise_for_status()
+    except requests.HTTPError as exc:
+        print("Dado não encontrado, continuando.")
+        cdi = None
+    except Exception as exc:
+        print("Erro, parando a execução.")
+        raise exc
+    else:
+        dado = json.loads(response.text)
+        cdi = float(dado['taxa'].replace(',', '.')) + (random() - 0.5)
+
+    if os.path.exists('./taxa-cdi.csv') == False:
+        with open(file='./taxa-cdi.csv', mode='w', encoding='utf8') as fp:
+            fp.write('data,hora,taxa\n')
+
+    with open(file='./taxa-cdi.csv', mode='a', encoding='utf8') as fp:
+        fp.write(f'{data},{hora},{cdi}\n')
+
+    time.sleep(2 + (random() - 0.5))
+
+print("Sucesso")
+
+df = pd.read_csv('./taxa-cdi.csv')
+
+grafico = sns.lineplot(x=df['hora'], y=df['taxa'])
+_ = grafico.set_xticklabels(labels=df['hora'], rotation=90)
+grafico.get_figure().savefig(f"{argv[1]}.png")
